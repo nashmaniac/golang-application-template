60268792a0ec00937961d2e76f4a5499809b1743
Raju Ahmed Shetu <shetu2153@gmail.com>
2022-04-12 11:11:19 -0400

    added the first sets of unit tests

--- usecases/usecases_suite_test.go
+++ usecases/usecases_suite_test.go
@@ -3,11 +3,20 @@ package usecases_test
 import (
 	"testing"
 
+	"github.com/golang/mock/gomock"
+	"github.com/nashmaniac/golang-application-template/mocks"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
+var mockPersistenceStore *mocks.MockPeristenceStore
+var mockController *gomock.Controller
+
 func TestUsecases(t *testing.T) {
 	RegisterFailHandler(Fail)
+
+	mockController = gomock.NewController(GinkgoT())
+	mockPersistenceStore = mocks.NewMockPeristenceStore(mockController)
+
 	RunSpecs(t, "Usecases Suite")
 }
